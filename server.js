8ed95f729fbfdef04f1ef91cb978d1219fdcb7e5
psbsj2016 <psbsj.2016@gmail.com>
2026-02-24 15:19:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -42,10 +42,15 @@ app.use((req, res, next) => {
 // =========================================================
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false, // false porque usa a porta 587 (TLS)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        ciphers: 'SSLv3'
     }
 });
 
