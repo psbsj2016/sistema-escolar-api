c574a4e194d10fb893a75e8e1ec737df8a0bb742
psbsj2016 <psbsj.2016@gmail.com>
2026-05-08 22:18:57 -0300

    build: atualizacao do projeto

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const dominiosPermitidos = [
     'https://www.sistemaptt.com.br',
     'https://sistemaptt.com.br',
     'http://localhost:3000',
+    'http://localhost:5173',
     'http://127.0.0.1:5500',
     'null'
 ];
