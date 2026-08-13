cd34d1045902227e0e614552a4c93c5c5da8978d
psbsj2016 <psbsj.2016@gmail.com>
2026-08-13 15:17:14 -0300

    feat(materiais): refatora o visualizador para prevenir sobreposicao de layout usando flexbox e aplica modo slide show imersivo em apresentacoes

--- src/routes/avaliacoesRoutes.js
+++ src/routes/avaliacoesRoutes.js
@@ -373,7 +373,7 @@ router.post('/:id/entregar', async (req, res) => {
                     escolaId: escola,
                     destinatarioNome: autorDaProva,
                     remetenteNome: nomeDoAluno,
-                    mensagem: `entrou na sessão ao vivo: "${provaOriginal.titulo}".`,
+                    mensagem: `iniciou avaliação: <strong>"${provaOriginal.titulo}"</strong>.`,
                     origem: 'tarefa', 
                     origemId: provaOriginal.id,
                     destinoNome: 'Avaliação',
